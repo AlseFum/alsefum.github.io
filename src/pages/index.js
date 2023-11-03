93b68c943d2ac96ffdd56e76bbaa6206a4e73539
胡龄 <2652328248@qq.com>
2023-11-03 12:52:13 +0800

    20231103

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import { createRouter,createWebHistory } from "vue-router";
+import { createRouter,createWebHashHistory } from "vue-router";
 import idle from './idle.vue'
 import emulator     from './emulator.vue'
 export default createRouter({
-    routes:[{path:"/idle",component:idle},{path:"/emulator",component:emulator}],
-    history:createWebHistory()
+    routes:[{path:"#/idle",component:idle},{path:"#/emulator",component:emulator}],
+    history:createWebHashHistory()
 })
