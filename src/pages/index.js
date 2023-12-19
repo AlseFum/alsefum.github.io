26069805e57bf332762bed9f118d70fb3037d994
胡龄 <2652328248@qq.com>
2023-12-19 13:20:44 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import dice from './dice.vue'
-import idleSim from '../idle/idleSim.vue'
+//import idleSim from '../idle/idleSim.vue'
 // import emulator from '../emulation/emulator.vue'
 export default createRouter({
     routes: [
         { path: "/dice", component: dice, name: "Dice" },
         // { path: "/emulator", component: emulator, name: "Emulator" },
-        { path: "/idlesim", component: idleSim, name: "IdleSim" }],
+    //    { path: "/idlesim", component: idleSim, name: "IdleSim" }
+    ],
     history: createWebHashHistory()
 })
